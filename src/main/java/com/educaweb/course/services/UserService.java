b0f26d664e0f6f572e55da1aae3bb703a2fec159
amercador <amercador@hotmail.com>
2021-03-02 16:12:23 -0300

    Exception handling - delete

--- src/main/java/com/educaweb/course/services/UserService.java
+++ src/main/java/com/educaweb/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.educaweb.course.entities.User;
 import com.educaweb.course.repositories.UserRepository;
+import com.educaweb.course.services.exceptions.DatabaseException;
 import com.educaweb.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -34,7 +37,13 @@ public class UserService {
 	
 	//metodo para deletar um usuario
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	//metodo para atualizar um usuario
