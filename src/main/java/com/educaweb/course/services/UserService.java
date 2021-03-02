4782e1e606c55c87c3b6fe2a1e06ee71096a4b78
amercador <amercador@hotmail.com>
2021-03-02 12:34:33 -0300

    Exception handling - findById

--- src/main/java/com/educaweb/course/services/UserService.java
+++ src/main/java/com/educaweb/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.educaweb.course.entities.User;
 import com.educaweb.course.repositories.UserRepository;
+import com.educaweb.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -23,7 +24,7 @@ public class UserService {
 	//metodo para buscar o usuário por id
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	//metodo para inserir um usuario
